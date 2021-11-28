92d210fb65e96023dba0f92fcb22b25a360637ec
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-11-28 03:00:58 -0800

    stop invite loading animation on failure

--- web/src/pages/AdminPage.tsx
+++ web/src/pages/AdminPage.tsx
@@ -23,41 +23,51 @@ function CreateInvite() {
         const inviteLink = `${window.location.origin}/signup?invite_token=${inviteToken.invite_token}`
         setState({inviteLink: inviteLink, isLoading: false})
       })
-      .catch(() => {message.error('Failed to create invite.')})
+      .catch(() => {
+        message.error('Failed to create invite.')
+        setState({isLoading: false});
+      })
+  }
+
+  const onCopy = () => {
+    if (state.inviteLink) {
+      navigator.clipboard.writeText(state.inviteLink)
+      message.success("Invite Link Copied", .5)
+    }
   }
   
   return (
     <>
-    <Input.Group>
-    <Input
-      value={state.inviteLink}
-      style={{ width: 'calc(100% - 100px)' }}/>
-    {state.isLoading &&
-        <Button icon={<ReloadOutlined spin/>}/>
-    }
-    {!state.inviteLink &&
-      <Tooltip title="Generate New Invite">
-        <Button icon={<ReloadOutlined />} onClick={getToken}/>
-      </Tooltip>
-    }
-    {state.inviteLink &&
-      <Tooltip title="Copy Invite Link">
-        <Button 
-          icon={<CopyOutlined />}
-          onClick={() => state.inviteLink && navigator.clipboard.writeText(state.inviteLink)} />
-      </Tooltip>
-    }
-  </Input.Group>
-  <div style={{paddingTop:'5px'}}><Typography.Text type="secondary"><InfoCircleOutlined/> Invite links expire after 24 Hours</Typography.Text></div>
+      <Input.Group>
+        <Input
+          value={state.inviteLink}
+          style={{ width: 'calc(100% - 100px)' }}/>
+          {state.isLoading &&
+              <Button icon={<ReloadOutlined spin/>}/>
+          }
+          {!state.inviteLink && !state.isLoading &&
+            <Tooltip title="Generate New Invite">
+              <Button icon={<ReloadOutlined />} onClick={getToken}/>
+            </Tooltip>
+          }
+          {state.inviteLink &&
+            <Tooltip title="Copy Invite Link">
+              <Button 
+                icon={<CopyOutlined />}
+                onClick={onCopy} />
+            </Tooltip>
+          }
+      </Input.Group>
+      <div style={{paddingTop:'5px'}}>
+        <Typography.Text type="secondary"><InfoCircleOutlined/> Invite links expire after 24 Hours</Typography.Text>
+      </div>
   </>
   
   );
 }
 
 export default function AdminPage() {
-
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate()  
   return (
     <>
       <PageHeader title='Admin Panel' onBack={() => navigate('/')}/>
