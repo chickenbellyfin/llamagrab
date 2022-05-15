d6d31b426473c743ec339a31d3ef39b3c50e51e9
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-05-14 22:49:01 -0700

    back button for region page

--- web/src/pages/RegionsPage.tsx
+++ web/src/pages/RegionsPage.tsx
@@ -1,12 +1,15 @@
 import { PageHeader } from 'antd'
+import { useNavigate } from 'react-router-dom';
 import ContentWrapper from '../components/ContentWrapper';
 import RegionStatusSection from '../components/RegionStatusSection';
 
 export default function RegionsPage() {
+  const navigate = useNavigate();
   return (
     <ContentWrapper>
       <PageHeader
-          title={<span className="ui-title">Region Statuses</span>}/>
+          title={<span className="ui-title">Region Statuses</span>}
+          onBack={() => navigate('/')}/>
       <RegionStatusSection/>
     </ContentWrapper>
   );
