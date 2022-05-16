9fb513ed5f327d139630e8b40752b26e4bc21dfd
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-05-16 12:17:32 -0700

    getBreakpoint()

---
+++ web/src/util.ts
@@ -0,0 +1,11 @@
+import { ScreenMap } from "antd/lib/_util/responsiveObserve";
+
+export function getBreakpoint(breakpoint: ScreenMap): string {
+    var b = 'xs';
+    b = breakpoint.sm ? 'sm': b;
+    b = breakpoint.md ? 'md': b;
+    b = breakpoint.lg ? 'lg': b;
+    b = breakpoint.xl ? 'xl': b;
+    b = breakpoint.xxl ? 'xxl': b;
+    return b;
+}
