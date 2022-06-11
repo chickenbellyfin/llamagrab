7496c98ac73ece3209f505fe1611a0d29b3d4cbf
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-06-10 22:11:55 -0700

    improve agent setup script

--- agent/scripts/setup.sh
+++ agent/scripts/setup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -ex
 
-# Install docker & docker-compose
+# Install docker
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
 if [ -z $(which docker) ]; then
   curl -L "https://get.docker.com" -o "get-docker.sh"
   sudo sh get-docker.sh && rm get-docker.sh
   # setup current user for docker
   sudo usermod -aG docker $USER
-  newgrp docker
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
 else
   echo "Docker is already installed, skipping"
 fi
 
+# Install docker-compose
+sudo apt update && sudo apt install python3-pip -y
+sudo pip install docker-compose
+
+newgrp docker <<'EOF'
 # get taserver image
 docker pull public.ecr.aws/i2q9d4v7/llamagrab-agent
 docker pull public.ecr.aws/i2q9d4v7/taserver
-
+EOF
