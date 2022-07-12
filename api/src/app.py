6787d2e4b539b154c7ae8c1d16d4f7d5e5540825
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-07-11 22:47:38 -0700

    disable cache header for index

--- api/src/app.py
+++ api/src/app.py
@@ -64,9 +64,17 @@ logging.basicConfig(handlers=[InterceptHandler()], level=0)
 # https://stackoverflow.com/a/68363904
 class SPAStaticFiles(StaticFiles):
   async def get_response(self, path: str, scope):
+      is_index = False
+      if path == '.' or path == 'index.html':
+        is_index = True
       response = await super().get_response(path, scope)
       if response.status_code == 404:
+          is_index = True
           response = await super().get_response('.', scope)
+
+      # for SPAs, index should never be cached
+      if is_index:
+        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
       return response
 
 
