bc094d9f3a77c115bf14ccc55b31d85a8852c6c2
chickenbellyfin <chickenbellyfinn@gmail.com>
2023-08-25 15:42:05 -0700

    modal container, inline some JS with hx-on

--- web2/public/index.js
+++ web2/public/index.js
@@ -2,20 +2,10 @@ import 'htmx.org';
 import $ from 'jquery';
 
 $(function () {
-  // slide side-menu in and out on mobile
-  $('#expand-button').on('click', function(event) {
-    $('#side-menu').toggleClass('active');
-  });
-
   // close side-menu when a link is clicked
   $('#side-menu a').on('click', function(event){
     $('#side-menu').removeClass('active');
   })
-
-  // dismiss all modals when background is clicked
-  $(document).on('click', '.modal-background', function(event) {
-    $(event.target.closest('.modal')).remove();
-  });
 
   // Listen for `Hx-Trigger: {"toast": ...}` from the server and show a popup at the top of the screen
   $(document).on('toast', function(event) {
