fd9ba5523dbcdfa4229b57177b68781a5f38b3d8
chickenbellyfin <chickenbellyfinn@gmail.com>
2023-08-25 15:58:05 -0700

    remove jquery

--- web2/public/index.js
+++ web2/public/index.js
@@ -1,17 +1,19 @@
 import 'htmx.org';
-import $ from 'jquery';
 
-$(function () {
+document.addEventListener("DOMContentLoaded", function(){
   // close side-menu when a link is clicked
-  $('#side-menu a').on('click', function(event){
-    $('#side-menu').removeClass('active');
-  })
+  document.querySelectorAll("#side-menu a").forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      document.getElementById("side-menu").classList.remove("active");
+    });
+  });
 
   // Listen for `Hx-Trigger: {"toast": ...}` from the server and show a popup at the top of the screen
-  $(document).on('toast', function(event) {
+  document.body.addEventListener("toast", function(event){
     const eventClass = event.detail.type == "success" ? "is-success" : "is-danger";
-    const toast = $(`<div class="notification ${eventClass}">${event.detail.message}</div>`);
-    $('#toast-container').append(toast);
+    const template = document.createElement('template');
+    template.innerHTML = `<div class="notification ${eventClass}">${event.detail.message}</div>`;
+    document.getElementById('toast-container').append(...template.content.children);
     setTimeout(() => {toast.remove()}, 3000);
-  });
+  })
 });
