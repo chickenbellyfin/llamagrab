df6682209aa682274691cff6cdd67c97b885bfad
Giga <gigabyte5671@gmail.com>
2026-08-28 21:15:27 +1200

    Update the React render API from v17 to v18

--- web/src/index.js
+++ web/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ProvideAuth } from './auth';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+
+root.render(
   <ProvideAuth>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-  </ProvideAuth>,
-  document.getElementById('root')
+  </ProvideAuth>
 );
 
 // If you want to start measuring performance in your app, pass a function
